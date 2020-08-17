241bb4d5d57db06169a47047b7d0396058bb3fab
ChenRui927 <1152186572@qq.com>
2020-08-17 11:24:49 +0800

    完成了报表

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,34 +11,26 @@ module.exports = {
     parser: 'babel-eslint',
     // parser: 'vue-eslint-parser',
   },
+  globals: {
+    'initNECaptcha': true
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-tabs': 0,
-    'no-mixed-spaces-and-tabs': 0,
-    'indent':['off','tab'],
-    'no-trailing-spaces': 0,
-    'space-before-function-paren':0,
-    'no-unused-vars': [0, {
+    'no-unused-vars': [2, {
       // 允许声明未使用变量
       'vars': 'local',
       // 参数不检查
       'args': 'none'
     }],
+    'no-unused-vars' : 0,
+    'no-tabs': 0,
+    'no-mixed-spaces-and-tabs': 0,
+    'indent':['off','tab'],
+    'no-trailing-spaces': 0,
+    'space-before-function-paren':0,
      "no-unused-vars": "off",
      "camelcase": [0, {"properties": "never"}]
     //  "camelcase": ["error", {"allow": ["aa_bb"]}],
-      // test:/\.js$/,
-      // use: [
-      //   {
-      //     loader: "babel-loader",
-      //     options: {
-      //       presets: ['env']
-      //     }
-      //   },
-      //   "eslint-loader"
-      // ],
-      // exclude: __dirname + 'node_modules',
-      // include: __dirname + 'src'
   }
 }
